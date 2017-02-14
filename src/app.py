470c3a6d47252310191c9fea6d7f15286e4f6628
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-14 20:58:42 +0100

    Added logger todo and list loader

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
 from loaders.configLoader import ConfigLoader
 
+
+# set up logger
+# TODO logger
+
+# initial configuration load
 configloader = ConfigLoader()
 commands = configloader.getcommands()
+lists = configloader.getlists()
