9ae6fd13420c4924c53fbe0480a5377305967ace
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-18 21:09:44 +0100

    giving log directly

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -6,5 +6,5 @@ class Controller:
     def __init__(self, log=Log()):
         self.log = log
 
-    def execute(self, command, arg, user):
-        getattr(self, command)(arg=arg, user=user)
+    def execute(self, command, arg, user, lists, log, telnet):
+        getattr(self, command)(arg, user, lists, log, telnet)
