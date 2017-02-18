2aaf011a2436c720579de94fa2f1b9aa22cda059
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-18 21:10:04 +0100

    changed to get telnet directly

--- src/util/command.py
+++ src/util/command.py
@@ -3,18 +3,27 @@ from util.log import Log
 
 
 class Command:
-    def __init__(self, key, controller, description="No description provided.", restriction="none", log=Log()):
+    def __init__(self, key, controller, reqargs, description="No description provided.", restriction="none"):
         self.key = key
+        self.reqArgs = reqargs
         self.controller = controller
         self.description = description
         self.restriction = restriction
-        self.lists={}
 
         _controllerDir = self.controller.split(".")
         _moduleName = _controllerDir[0]
         self.controllerName = _controllerDir[1]
         _module = importlib.import_module("controllers.{0}".format(_moduleName))
-        self.controllerClass = getattr(_module, _moduleName.capitalize())(log=log)
+        self.controllerClass = getattr(_module, _moduleName.capitalize())()
 
-    def execute(self, arg=None, user=None):
-        self.controllerClass.execute(self.controllerName, arg, user)
+    def execute(self, user, log, lists, telnet, arg=None):
+        if "none" not in self.restriction:
+            if user not in lists[self.restriction].getusers(telnet):
+                log.warn("{user} is not permitted to use {cmd}".format(user=user, cmd=self.controllerName), tag="command")
+                return
+
+        if self.reqArgs and not bool(arg):
+            log.warn("{user} called {cmd} which requires arguments".format(user=user, cmd=self.controllerName), tag="command")
+            return
+
+        self.controllerClass.execute(self.controllerName, arg, user, lists, log, telnet)
