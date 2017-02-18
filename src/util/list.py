bdf282465ad8ed63cf1a6636106faf2914415b17
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-18 21:09:54 +0100

    get list from telnet

--- src/util/list.py
+++ src/util/list.py
@@ -1,13 +1,22 @@
-from util.log import Log
-
-
 class List:
 
-    def __init__(self, key, listid, description="No description provided.", log=Log()):
+    def __init__(self, key, listid, log, description="No description provided."):
         self.key = key
         self.listid = listid
+        self.log = log
         self.description = description
-        self.players = {}
+        self.user = {}
 
-    def getplayers(self):
-        return self.players
+    def getusers(self, telnet):
+        telnet.command("={listid}".format(listid=self.listid))
+        data = telnet.readuntil("-- {listid} list:".format(listid=self.listid))
+        data = telnet.readuntil("\n")
+        data = telnet.readuntil("fics").replace("\n", " ").replace("\r", " ").replace("fics","").split(" ")
+        data = filter(None, data)
+        self.user = data
+        return self.user
+
+    def adduser(self, user, telnet):
+        if " " not in user and user:
+            telnet.write("+{listid} {user}".format(listid=self.listid, user=user))
+            self.user.add(user)
