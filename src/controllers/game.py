a94e805305139c4db6c7f55c32862658f1da2f64
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-21 22:14:10 +0100

    made method static

--- src/controllers/game.py
+++ src/controllers/game.py
@@ -2,11 +2,16 @@ from controllers.controller import Controller
 
 
 class Game(Controller):
+    @staticmethod
     def join(self, arg, user, lists, log, telnet):
         log.info("{user} tries to join".format(user=user))
 
-    def leave(self, arg, user, lists, log, telnet):
-        print("{0} wants to leave".format(user))
+    @staticmethod
+    def leave(arg, user, lists, log, telnet):
+        lists.add_user(user)
+        telnet.write("tell {0} You have joined the team. Type /help for more information.".format(user))
+        log.info("{0} left".format(user), "game.leave")
 
+    @staticmethod
     def vote(self, arg, user, lists, log, telnet):
         print("{0} voted {1}".format(user, arg))
