48bc837da9bde5b9cd60dfd467f2bf743caebc46
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-21 22:15:19 +0100

    it's an array not dict

--- src/util/list.py
+++ src/util/list.py
@@ -1,22 +1,26 @@
 class List:
-
     def __init__(self, key, listid, log, description="No description provided."):
         self.key = key
         self.listid = listid
         self.log = log
         self.description = description
-        self.user = {}
+        self.users = []
 
-    def getusers(self, telnet):
-        telnet.command("={listid}".format(listid=self.listid))
+    def get_users(self, telnet):
+        telnet.write("={listid}".format(listid=self.listid))
         data = telnet.readuntil("-- {listid} list:".format(listid=self.listid))
         data = telnet.readuntil("\n")
         data = telnet.readuntil("fics").replace("\n", " ").replace("\r", " ").replace("fics","").split(" ")
         data = filter(None, data)
-        self.user = data
-        return self.user
+        self.users = data
+        return self.users
 
-    def adduser(self, user, telnet):
+    def add_user(self, user, telnet):
         if " " not in user and user:
             telnet.write("+{listid} {user}".format(listid=self.listid, user=user))
-            self.user.add(user)
+            self.users.append(user)
+
+    def remove_user(self, user, telnet):
+        if " " not in user and user and user in self.users:
+            telnet.write("-{listid} {user}".format(listid=self.listid, user=user))
+            self.users.remove(user)
