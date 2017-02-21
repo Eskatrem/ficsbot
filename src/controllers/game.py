b9556b92d82fe81f7e9cd9ddc2d32a6ffb5ea45a
Jonas Drotleff <jonas.drotleff@gmail.com>
2017-02-21 22:14:35 +0100

    completed join and vote controller

--- src/controllers/game.py
+++ src/controllers/game.py
@@ -4,7 +4,9 @@ from controllers.controller import Controller
 class Game(Controller):
     @staticmethod
     def join(self, arg, user, lists, log, telnet):
-        log.info("{user} tries to join".format(user=user))
+        lists.add_user(user, telnet)
+        telnet.write("tell {0} You have joined the team. Type /help for more information.".format(user))
+        log.info("{0} joined".format(user), "game.join")
 
     @staticmethod
     def leave(arg, user, lists, log, telnet):
@@ -14,4 +16,4 @@ class Game(Controller):
 
     @staticmethod
     def vote(self, arg, user, lists, log, telnet):
-        print("{0} voted {1}".format(user, arg))
+        log.info("{0} voted for {1}".format(user, arg[0]), "game.vote")
